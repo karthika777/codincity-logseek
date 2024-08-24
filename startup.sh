ee2c0f7fcd689af24f14a840f08221350acad365
karthika777 <100334817+karthika777@users.noreply.github.com>
2024-08-24 19:41:59 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
-python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt || { echo "Failed to install dependencies"; exit 1; }
+python app.py || { echo "Failed to start the application"; exit 1; }
 
-python app.py
