c467223ad113105fa3d921f46a9e93d3c2bf4b03
karthika777 <100334817+karthika777@users.noreply.github.com>
2024-08-25 20:35:51 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,12 @@
-python -m pip install -r requirements.txt || { echo "Failed to install dependencies"; exit 1; }
-python app.py || { echo "Failed to start the application"; exit 1; }
 
+
+# Install the dependencies
+echo "Installing dependencies..."
+python -m pip install --no-cache-dir -r requirements.txt || { echo "Failed to install dependencies"; exit 1; }
+
+# Fetch the PORT environment variable from Azure App Service or default to 8000
+PORT=${PORT:-8000}
+
+# Start the application
+echo "Starting the application on port $PORT..."
+python app.py --port=$PORT || { echo "Failed to start the application"; exit 1; }
